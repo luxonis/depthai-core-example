e96c655a95b6dedfc57d68290d633ca7df731c38
Martin Peterlin <martin.peterlin7@gmail.com>
2021-02-07 02:16:33 +0100

    Updated the example for MSVC

--- src/main.cpp
+++ src/main.cpp
@@ -18,28 +18,34 @@ int main(){
     colorCam->setInterleaved(true);
     colorCam->preview.link(xlinkOut->input);
     
-    // Connect to device
-    dai::Device device(pipeline);
 
-    // Get output queue
-    auto preview = device.getOutputQueue("preview");
+    try {
+        // Try connecting to device
+        dai::Device device(pipeline);
 
-    // Start pipeline
-    device.startPipeline();
+        // Get output queue
+        auto preview = device.getOutputQueue("preview");
 
-    cv::Mat frame;
-    while(true){
-        
-        // Receive 'preview' frame from device 
-        auto imgFrame = preview->get<dai::ImgFrame>();
-        
-        // Show the received 'preview' frame
-        cv::imshow("preview", cv::Mat(imgFrame->getHeight(), imgFrame->getWidth(), CV_8UC3, imgFrame->getData().data()));
-        
-        // Wait and check if 'q' pressed
-        if(cv::waitKey(1) == 'q') return 0;
-  
+        // Start pipeline
+        device.startPipeline();
+
+        cv::Mat frame;
+        while (true) {
+
+            // Receive 'preview' frame from device 
+            auto imgFrame = preview->get<dai::ImgFrame>();
+
+            // Show the received 'preview' frame
+            cv::imshow("preview", cv::Mat(imgFrame->getHeight(), imgFrame->getWidth(), CV_8UC3, imgFrame->getData().data()));
+
+            // Wait and check if 'q' pressed
+            if (cv::waitKey(1) == 'q') return 0;
+
+        }
+    } catch (const std::runtime_error& err) {
+        std::cout << err.what() << std::endl;
     }
+    
 
     return 0;
 }
